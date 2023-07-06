c29e926090bab5a6a65730fd2fdf7d53f8b7c43e
aeon <jerry73204@gmail.com>
2023-07-06 16:24:36 -0700

    Change GUID display format

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,7 +1,7 @@
 mod defrag_buf;
 
 pub use defrag_buf::DefragBuf;
-use rustdds::GUID;
+use rustdds::{structure::guid::EntityKind, GUID};
 use std::{
     fmt::{self, Display},
     mem::ManuallyDrop,
@@ -27,12 +27,34 @@ pub struct GUIDDisplay<'a>(&'a GUID);
 impl<'a> Display for GUIDDisplay<'a> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let GUID { prefix, entity_id } = self.0;
+
+        use EntityKind as E;
+
+        let entity_kind_str = match entity_id.entity_kind {
+            E::UNKNOWN_USER_DEFINED => "U-D",
+            E::WRITER_WITH_KEY_USER_DEFINED => "WKD",
+            E::WRITER_NO_KEY_USER_DEFINED => "WND",
+            E::READER_NO_KEY_USER_DEFINED => "RND",
+            E::READER_WITH_KEY_USER_DEFINED => "RKD",
+            E::WRITER_GROUP_USER_DEFINED => "WGD",
+            E::READER_GROUP_USER_DEFINED => "RGD",
+            E::UNKNOWN_BUILT_IN => "U-B",
+            E::PARTICIPANT_BUILT_IN => "P-B",
+            E::WRITER_WITH_KEY_BUILT_IN => "WKB",
+            E::WRITER_NO_KEY_BUILT_IN => "WNB",
+            E::READER_NO_KEY_BUILT_IN => "RNB",
+            E::READER_WITH_KEY_BUILT_IN => "RKB",
+            E::WRITER_GROUP_BUILT_IN => "WGB",
+            E::READER_GROUP_BUILT_IN => "RGB",
+            _ => unreachable!(),
+        };
+
         write!(
             f,
-            "{}{}{:02x}",
+            "{}|{}|{}",
             hex::encode(prefix.bytes),
             hex::encode(entity_id.entity_key),
-            entity_id.entity_kind.0,
+            entity_kind_str,
         )
     }
 }
